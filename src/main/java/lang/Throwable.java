5e1a1a55602eec4db0957ed3092f1f67fac58fa5
Ian Hopkins <ian@checkout51.com>
2013-11-13 12:01:48 -0500

    Adding exception classes.

---
+++ src/main/java/lang/Throwable.java
@@ -0,0 +1,48 @@
+package java.lang;
+
+public class Throwable() {
+
+	protected String message;
+	protected Throwable cause = this;
+	protected StackTraceElement[] stackTrace;
+
+	public Throwable() {
+		this.message = null;
+		fillInStackTrace();
+	}
+	
+	public Throwable(String message) {
+		this.message = message;
+		fillInStackTrace();
+	}
+	
+	public String toString() {
+		return getClass().getName() + ":" + getMessage();
+	}
+	
+	public String getMessage() {
+		if (message == null) {
+			return "null";
+		} else {
+			return message;
+		}
+	}
+	
+	public Throwable fillInStackTrace() {
+	
+	}
+	
+	public void printStackTrace() {
+		printStackTrace(System.err);
+	}
+	
+	public void printStackTrace(java.io.PrintStream s) {
+		s.println(getClass().getName());
+		if (stackTrace != null) {
+			for(int i = 0; i < stackTrace.length; i++) {
+				s.print("\tat ");
+				s.println(stackTrace[i]);
+			}
+		}
+	}
+}
