0475b045e1507dea41b9a59862e74b65cebccf14
Ian Hopkins <ian@checkout51.com>
2013-11-14 19:08:33 -0500

    Not part of spec, but thrown by the ClassLoader.

---
+++ src/main/java/java/lang/ClassFormatException.java
@@ -0,0 +1,11 @@
+package java.lang;
+
+public class ClassFormatException extends Exception {
+	public ClassFormatException() {
+		super();
+	}
+	
+	public ClassFormatException(String message) {
+		super(message);
+	}
+}
