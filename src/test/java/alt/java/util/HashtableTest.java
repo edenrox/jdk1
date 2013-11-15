828eef7bc4c648434e038273d4944278abddb742
Ian Hopkins <ian@checkout51.com>
2013-11-15 09:10:40 -0500

    Adding more tests for Stack and Hashtable

---
+++ src/test/java/alt/java/util/HashtableTest.java
@@ -0,0 +1,93 @@
+package alt.java.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HashtableTest {
+	
+	Hashtable colors;
+	
+	@Before
+	public void initTables() {
+		colors = new Hashtable();
+		colors.put("red", "#FF0000");
+		colors.put("green", "#00FF00");
+		colors.put("blue", "#0000FF");
+		colors.put("white", "#FFFFFF");
+		colors.put("black", "#000000");
+	}
+
+	@Test
+	public void testConstructors() {
+		
+	}
+	
+	@Test
+	public void testSize() {
+		Hashtable ht = new Hashtable();
+		
+		assertEquals(0, ht.size());
+		assertTrue(ht.isEmpty());
+		assertEquals(5, colors.size());
+		assertFalse(colors.isEmpty());
+	}
+	
+	@Test
+	public void testAddSetGet() {
+		
+		colors.put("magenta", "#FF00FF");
+		assertEquals(6, colors.size());
+		assertEquals("#FF00FF", colors.get("magenta"));
+		assertEquals("#FF0000", colors.get("red"));
+		
+		colors.put("red", "#FE0000");
+		assertEquals(6, colors.size());
+		assertEquals("#FE0000", colors.get("red"));
+	}
+	
+	@Test
+	public void testContains() {
+		assertFalse(colors.contains(null));
+		assertFalse(colors.containsKey(null));
+		
+		assertTrue(colors.containsKey("red"));
+		assertTrue(colors.contains("#FF0000"));
+		
+		assertFalse(colors.contains("greenyblue"));
+	}
+	
+	@Test
+	public void testClear() {
+		assertFalse(colors.isEmpty());
+		assertEquals("#FF0000", colors.get("red"));
+		
+		colors.clear();
+		assertTrue(colors.isEmpty());
+		assertEquals(null, colors.get("red"));
+		assertFalse(colors.contains("red"));
+	}
+	
+	@Test
+	public void testEnumerators() {
+		
+	}
+	
+	@Test
+	public void testOverflow() {
+		Hashtable ht = new Hashtable(1, 25.0f);
+		
+		ht.put("Gandalf", "wizard");
+		ht.put("Legolas", "elf");
+		
+		assertEquals(2, ht.size());
+		assertEquals("wizard", ht.get("Gandalf"));
+		assertEquals(null, ht.get("Smeagle"));
+		assertTrue(ht.containsKey("Legolas"));
+		assertTrue(ht.contains("elf"));
+		
+		assertEquals("{Legolas=elf, Gandalf=wizard}", ht.toString());
+	}
+
+}
---
+++ src/test/java/alt/java/util/StackTest.java
@@ -0,0 +1,86 @@
+package alt.java.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StackTest {
+	
+	Stack abc;
+	
+	@Before
+	public void initStacks() {
+		abc = new Stack();
+		abc.push("A");
+		abc.push("B");
+		abc.push("C");
+	}
+
+	@Test
+	public void testPushPeek() {
+		assertEquals(3, abc.size());
+	
+		assertEquals("C", abc.peek());
+		
+		abc.push("D");
+		assertEquals("D", abc.peek());
+	}
+	
+	@Test
+	public void testEmpty() {
+		assertFalse(abc.empty());
+		
+		abc.removeAllElements();
+		assertTrue(abc.empty());
+	}
+	
+	@Test
+	public void testPop() {
+		Object item = abc.pop();
+		
+		assertEquals("C", item);
+		assertEquals(2, abc.size());
+		
+		abc.push("D");
+		item = abc.pop();
+		
+		assertEquals("D", item);
+		assertEquals(2, abc.size());
+		
+		abc.pop();
+		abc.pop();
+		
+		assertTrue(abc.empty());
+		
+		
+		ExceptionHelper.assertThrows(EmptyStackException.class, new Runnable() {
+			public void run() {
+				abc.pop();
+			}
+		});
+		
+		ExceptionHelper.assertThrows(EmptyStackException.class, new Runnable() {
+			public void run() {
+				abc.peek();
+			}
+		});
+	}
+	
+	@Test
+	public void testSearch() {
+		assertEquals(1, abc.search("C"));
+		assertEquals(3, abc.search("A"));
+		assertEquals(-1, abc.search("E"));
+		
+		abc.push("D");
+		assertEquals(2, abc.search("C"));
+		assertEquals(1, abc.search("D"));
+		
+		abc.push("D");
+		assertEquals(3, abc.search("C"));
+		assertEquals(1, abc.search("D"));
+		assertEquals(-1, abc.search("E"));
+	}
+
+}
