6a338ae3660e85389fb901242548202cc7424656
Ian Hopkins <ian@checkout51.com>
2013-11-14 19:07:40 -0500

    Added check methods, not quite done yet.

--- src/main/java/java/lang/Character.java
+++ src/main/java/java/lang/Character.java
@@ -34,4 +34,95 @@ public class Character {
 	public char charValue() {
 		return value;
 	}
+	
+	
+	
+	
+	
+	public static boolean isDefined(char c) {
+		return false;
+	}
+	public static boolean isLowerCase(char c) {
+		return ((c >= 'a') && (c <= 'z'));
+	}
+	public static boolean isUpperCase(char c) {
+		return ((c >= 'A') && (c <= 'Z'));
+	}
+	public static boolean isTitleCase(char c) {
+		return false;
+	}
+	public static boolean isDigit(char c) {
+		return ((c >= '0') && (c <= '9'));
+	}
+	
+	public static boolean isLetter(char c) {
+		return (((c >= 'a') && (c <= 'z'))
+				|| ((c >= 'A') && (c <= 'Z')));
+	}
+	
+	public static boolean isLetterOrDigit(char c) {
+		return (isDigit(c) || isLetter(c));
+	}
+	
+	public static boolean isJavaLetter(char c) {
+		return (isLetter(c)
+				|| (c == '$')
+				|| (c == '_'));
+	}
+	
+	public static boolean isJavaLetterOrDigit(char c) {
+		return (isJavaLetter(c) || isDigit(c));
+	}
+	
+	public static boolean isSpace(char c) {
+		switch (c) {
+		case '\t':
+		case '\n':
+		case '\f':
+		case '\r':
+		case ' ':
+			return true;
+		default:
+			return false;
+		}
+	}
+	
+	private static boolean isValidChar(char ch, int radix) {
+		
+	}
+	
+	private static boolean isValidRadix(int radix) {
+		return ((radix >= MIN_RADIX) && (radix <= MAX_RADIX));
+	}
+	
+	private static boolean isValidDigit(int digit, int radix) {
+		return ((digit >= 0) && (digit < radix));
+	}
+	
+	public static int digit(char ch, int radix) {
+		if (!isValidRadix(radix)) {
+			return -1;
+		}
+		if (!isValidChar(ch, radix)) {
+			return -1;
+		}
+		
+		
+	}
+	
+	public static char forDigit(int digit, int radix) {
+		if (!isValidRadix(radix)) {
+			return '\0';
+		}
+		if (!isValidDigit(digit, radix)) {
+			return '\0';
+		}
+		
+		// Calculate the character for this digit
+		if (digit < 10) {
+			return (char) ('0' + digit);
+		} else {
+			return (char) ('a' + digit);
+		}
+	}
 }
