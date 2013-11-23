906d4ffdfcacf15ebac64747d9fffac5365a9ecc
Ian Hopkins <ian@checkout51.com>
2013-11-23 17:28:25 -0500

    Working on String and StringBuffer classes.

--- src/main/java/alt/java/lang/String2.java
+++ src/main/java/alt/java/lang/String2.java
@@ -12,41 +12,43 @@ public class String2 {
 
 	protected int offset;
 	protected int size;
-	protected char[] value;
+	protected char[] buffer;
 
 	public String2() {
-		init(EMPTY_BUFFER, 0, 0);
+		
+		// Create string that points to the empty buffer
+		buffer = EMPTY_BUFFER;
+		offset = 0;
+		size = 0;
 	}
 
-	public String2(String2 value) 
-			throws NullPointerException {
+	public String2(String2 value) throws NullPointerException {
 		if (value == null) {
 			throw new NullPointerException("value cannot be null");
 		}
-		init(value.value, value.offset, value.size);
+		
+		// Create a string that points to the underlying data of the other string
+		this.buffer = value.buffer;
+		this.offset = value.offset;
+		this.size = value.size;
 	}
-	public String2(char[] value) 
-			throws NullPointerException {
+	public String2(char[] value) throws NullPointerException {
+		this(value, 0, value.length);
+		
+	}
+	public String2(char[] value, int offset, int length) throws NullPointerException {
 		if (value == null) {
 			throw new NullPointerException("value cannot be null");
 		}
-		init(value, 0, value.length);
+		
+		// Declare a new buffer
+		this.buffer = new char[length];
+		this.offset = 0;
+		this.size = length;
+		
+		// Copy the characters into the new buffer
+		System.arraycopy(value, offset, this.buffer, 0, length);
 	}
-	public String2(char[] value, int offset, int count) 
-			throws NullPointerException {
-		if (value == null) {
-			throw new NullPointerException("value cannot be null");
-		}
-		init(value, offset, count);
-	}
-	
-	protected final void init(char[] value, int offset, int count) {
-		this.offset = offset;
-		this.size = count;
-		this.value = value;
-	}
-	
-
 
 	public int hashCode() {
 		return 0;
@@ -56,12 +58,11 @@ public class String2 {
 		return size;
 	}
 	
-	public char charAt(int index)
-			throws NullPointerException, IndexOutOfBoundsException {
+	public char charAt(int index) throws NullPointerException, IndexOutOfBoundsException {
 		if ((index < 0) || (index >= size)) {
 			throw new IndexOutOfBoundsException();
 		}
-		return value[offset + index];
+		return buffer[offset + index];
 	}
 	
 	public void getChars(int srcBegin, int srcEnd, char dst[], int dstBegin)
@@ -78,7 +79,17 @@ public class String2 {
 			throw new IndexOutOfBoundsException();
 		}
 
-		System.arraycopy(value, offset + srcBegin, dst, dstBegin, srcEnd - srcBegin);
+		System.arraycopy(buffer, offset + srcBegin, dst, dstBegin, srcEnd - srcBegin);
+	}
+	
+	public char[] toCharArray() {
+		char[] rv = new char[size];
+		System.arraycopy(buffer, offset, rv, 0, size);
+		return rv;
+	}
+	
+	public void getBytes(int srcBegin, int srcEnd, byte dst[], int dstBegin) {
+		// TODO: implement this
 	}
 	
 	public int indexOf(int ch) {
@@ -86,6 +97,8 @@ public class String2 {
 	}
 	
 	public int indexOf(int ch, int fromIndex) {
+		fromIndex = Math.max(0,  fromIndex);
+		fromIndex = Math.min(fromIndex, size);
 		for(int i = fromIndex; i < length(); i++) {
 			if (charAt(i) == ch) {
 				return 	i;
@@ -104,8 +117,12 @@ public class String2 {
 		if (str == null) {
 			throw new NullPointerException("str cannot be null");
 		}
+		fromIndex = Math.max(0,  fromIndex);
+		fromIndex = Math.min(fromIndex, size);
 		for(int i = offset + fromIndex; i < size - str.length(); i++) {
-			
+			if (regionMatches(i, str, 0, str.length())) {
+				return i;
+			}
 		}
 		return INDEX_NOT_FOUND;
 	}
@@ -115,6 +132,8 @@ public class String2 {
 	}
 	
 	public int lastIndexOf(char c, int fromIndex) {
+		fromIndex = Math.max(0,  fromIndex);
+		fromIndex = Math.min(fromIndex, size);
 		for(int i = fromIndex; i >= 0; i--) {
 			if (charAt(i) == c) {
 				return i;
@@ -133,8 +152,12 @@ public class String2 {
 		if (str == null) {
 			throw new NullPointerException();
 		}
+		fromIndex = Math.max(0,  fromIndex);
+		fromIndex = Math.min(fromIndex, size);
 		for(int i = fromIndex; i >= 0; i--) {
-			
+			if (regionMatches(i, str, 0, str.length())) {
+				return i;
+			}
 		}
 		return INDEX_NOT_FOUND;
 	}
@@ -162,7 +185,7 @@ public class String2 {
 		}
 		
 		int newLength = endIndex - beginIndex;
-		return new String2(value, beginIndex, newLength);
+		return new String2(buffer, beginIndex, newLength);
 	}
 		
 	
@@ -179,8 +202,8 @@ public class String2 {
 		char[] buffer = new char[this.length() + str.length()];
 		
 		// Copy the old strings into the new buffer
-		System.arraycopy(value, offset, buffer, 0, size);
-		System.arraycopy(str.value, str.offset, buffer, this.size, str.size);
+		System.arraycopy(buffer, offset, buffer, 0, size);
+		System.arraycopy(str.buffer, str.offset, buffer, this.size, str.size);
 		
 		// Create a new string object with the new buffer
 		return new String2(buffer);
@@ -192,12 +215,12 @@ public class String2 {
 		char[] buffer = new char[this.length()];
 		
 		// Copy the string into the new buffer
-		System.arraycopy(value, offset, buffer, 0, size);
+		System.arraycopy(buffer, offset, buffer, 0, size);
 		
 		// Replace characters in the new buffer
 		for(int i = 0; i < buffer.length; i++) {
-			if (value[i] == oldChar) {
-				value[i] = newChar;
+			if (buffer[i] == oldChar) {
+				buffer[i] = newChar;
 			}
 		}
 		
@@ -234,7 +257,7 @@ public class String2 {
 			return EMPTY_STRING;
 		} else {
 			// Return the trimmed string
-			return new String2(value, startIndex, endIndex - startIndex);
+			return new String2(buffer, startIndex, endIndex - startIndex);
 		}
 	}
 	
@@ -272,11 +295,65 @@ public class String2 {
 		return true;
 	}
 	
+	public int compareTo(String anotherString) throws NullPointerException {
+		if (anotherString == null) {
+			throw new NullPointerException("cannot compare to null");
+		}
+		int max = Math.min(this.length(), anotherString.length());
+		for(int i = 0; i < max; i++) {
+			int diff = this.charAt(i) - anotherString.charAt(i);
+			if (diff != 0) {
+				// Difference in characters, so return it
+				return diff;
+			}
+		}
+		
+		// No difference in characters, so order by length
+		return this.length() - anotherString.length();
+	}
+	
 	public static final String2 fromString(String that) {
 		return new String2(that.toCharArray());
 	}
 	
 	public String toString() {
-		return new String(value, offset, size);
+		return new String(buffer, offset, size);
+	}
+	
+	public boolean regionMatches(int thisOffset, String2 other, int otherOffset, int length) {
+		return regionMatches(false, thisOffset, other, otherOffset, length);
+	}
+	
+	public boolean regionMatches(boolean ignoreCase, int thisOffset, String2 other, int otherOffset, int length) {
+		if (thisOffset+length > length()) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (otherOffset+length > other.length()) {
+			throw new IndexOutOfBoundsException();
+		}
+		for(int i = 0; i < length; i++) {
+			if (ignoreCase) {
+				if (Character.toLowerCase(charAt(thisOffset + i)) != Character.toLowerCase(other.charAt(otherOffset + i))) {
+					return false;
+				}
+			} else {
+				if (charAt(thisOffset + i) != other.charAt(otherOffset + i)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public boolean startsWith(String2 prefix) throws NullPointerException {
+		return startsWith(prefix, prefix.length());
+	}
+	
+	public boolean startsWith(String2 prefix, int toLength) throws NullPointerException {
+		return regionMatches(0, prefix, 0, toLength);
+	}
+	
+	public boolean endsWith(String2 suffix) throws NullPointerException {
+		return regionMatches(length() - suffix.length(), suffix, 0, suffix.length());
 	}
 }
--- src/main/java/alt/java/lang/StringBuffer.java
+++ src/main/java/alt/java/lang/StringBuffer.java
@@ -1,6 +1,6 @@
 package alt.java.lang;
 
-import alt.java.lang.IndexOutOfBoundsException;
+import java.lang.*;
 
 public class StringBuffer {
 	private int size;
@@ -10,11 +10,7 @@ public class StringBuffer {
 		init(0);
 	}
 	
-	public StringBuffer(int length) 
-			throws NegativeArraySizeException {
-		if (length < 0) {
-			throw new NegativeArraySizeException("length cannot be negative");
-		}
+	public StringBuffer(int length) throws NegativeArraySizeException {
 		init(length);
 	}
 	
@@ -50,10 +46,7 @@ public class StringBuffer {
 		}
 	}
 	
-	public StringBuffer append(Object obj) {
-		append(obj.toString());
-		return this;
-	}
+	
 	
 	public StringBuffer append(String str) {
 		ensureCapacity(size + str.length());
@@ -61,57 +54,84 @@ public class StringBuffer {
 		size += str.length();
 		return this;
 	}
-	
-	public StringBuffer append(char[] str)
-			throws NullPointerException {
-		if (str == null) {
-			throw new NullPointerException("str cannot be null");
-		}
-		ensureCapacity(size + str.length);
-		System.arraycopy(str, 0, buffer, size, str.length);
-		size += str.length;
-		return this;
-	}
-	
-	public StringBuffer append(char[] str, int offset, int len) 
-			throws NullPointerException, IndexOutOfBoundsException {
-		if (str == null) {
-			throw new NullPointerException("str cannot be null");
-		}
-		if (offset < 0) {
-			throw new IndexOutOfBoundsException("offset must be positive");
-		}
-		if (offset > str.length) {
-			throw new IndexOutOfBoundsException("offset is greater than length");
-		}
-		if (offset + len > str.length) {
-			throw new IndexOutOfBoundsException("offset and length exceed array bound");
-		}
-		ensureCapacity(size + len);
-		System.arraycopy(str, offset, buffer, size, len);
-		size += len;
-		return this;
-	}
-	
 	public StringBuffer append(boolean b) {
-		if (b) {
-			return append("true");
-		} else {
-			return append("false");
-		}
+		return append(String.valueOf(b));
 	}
-	
 	public StringBuffer append(char c) {
 		ensureCapacity(size + 1);
 		buffer[size] = c;
 		size++;
 		return this;
 	}
-	
+	public StringBuffer append(char[] str)
+			throws NullPointerException {
+		ensureCapacity(size + str.length);
+		System.arraycopy(str, 0, buffer, size, str.length);
+		size += str.length;
+		return this;
+	}
+	public StringBuffer append(char[] str, int offset, int len) 
+			throws NullPointerException, IndexOutOfBoundsException {
+		ensureCapacity(size + len);
+		System.arraycopy(str, offset, buffer, size, len);
+		size += len;
+		return this;
+	}
+	public StringBuffer append(double d) {
+		return append(String.valueOf(d));
+	}
+	public StringBuffer append(float f) {
+		return append(String.valueOf(f));
+	}
 	public StringBuffer append(int i) {
-		return append(Integer.toString(i));
+		return append(String.valueOf(i));
+	}
+	public StringBuffer append(long l) {
+		return append(String.valueOf(l));
+	}
+	public StringBuffer append(Object obj) {
+		return append(String.valueOf(obj));
 	}
 	
+	
+	public StringBuffer insert(int offset, String str) {
+		if (offset > size) {
+			throw new IndexOutOfBoundsException();
+		}
+		ensureCapacity(size + str.length());
+		
+		// move the old chars after the insertion point
+		System.arraycopy(buffer, offset, buffer, offset + str.length(), size - offset);
+		
+		// move the new chars into the insertion point
+		str.getChars(offset, str.length(), buffer, offset);
+		
+		return this;
+	}
+	public StringBuffer insert(int offset, boolean b) {
+		return insert(offset, String.valueOf(b));
+	}
+	public StringBuffer insert(int offset, char c) {
+		return insert(offset, String.valueOf(c));
+	}
+	public StringBuffer insert(int offset, char str[]) {
+		return insert(offset, String.valueOf(str));
+	}
+	public StringBuffer insert(int offset, double d) {
+		return insert(offset, String.valueOf(d));
+	}
+	public StringBuffer insert(int offset, float f) {
+		return insert(offset, String.valueOf(f));
+	}
+	public StringBuffer insert(int offset, int i) {
+		return insert(offset, String.valueOf(i));
+	}
+	public StringBuffer insert(int offset, long l) {
+		return insert(offset, String.valueOf(l));
+	}
+	public StringBuffer insert(int offset, Object o) {
+		return insert(offset, String.valueOf(o));
+	}
 	
 	public StringBuffer reverse() {
 		int max = size / 2;
@@ -124,4 +144,30 @@ public class StringBuffer {
 		return this;
 	}
 	
+	public char charAt(int index) {
+		if (index >= size) {
+			throw new IndexOutOfBoundsException("index is beyond length of string buffer");
+		}
+		return buffer[index];
+	}
+	
+	public void setCharAt(int index, char ch) {
+		if (index >= size) {
+			throw new IndexOutOfBoundsException("index is beyond length of string buffer");
+		}
+		buffer[index] = ch;
+	}
+	
+	public void getChars(int srcBegin, int srcEnd, char dst[], int dstOffset) {
+		int length = srcEnd - srcBegin;
+		if (srcEnd >= size) {
+			throw new IndexOutOfBoundsException("srcEnd is beyond the length of string buffer");
+		}
+		System.arraycopy(buffer, srcBegin, dst, dstOffset, length);
+	}
+	
+	
+	public String toString() {
+		return new String(buffer, 0, size);
+	}
 }
