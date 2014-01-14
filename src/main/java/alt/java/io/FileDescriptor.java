6a99dd8fe27d65ab89f5009e07c25b326be0f873
Ian Hopkins <ian@checkout51.com>
2014-01-14 15:06:10 -0500

    Working on the FileDescriptor to track the underlying FD as an integer.

--- src/main/java/alt/java/io/FileDescriptor.java
+++ src/main/java/alt/java/io/FileDescriptor.java
@@ -1,11 +1,16 @@
 package alt.java.io;
 
 public final class FileDescriptor {
-	public static final FileDescriptor in = new FileDescriptor();
-	public static final FileDescriptor out = new FileDescriptor();
-	public static final FileDescriptor err = new FileDescriptor();
+	public static final FileDescriptor in = new FileDescriptor(0);
+	public static final FileDescriptor out = new FileDescriptor(1);
+	public static final FileDescriptor err = new FileDescriptor(2);
+	
+	private int fd;
+	private FileDescriptor(int fd) {
+		this.fd = fd;
+	}
 	
 	public boolean valid() {
-		return true;
+		return (fd != -1);
 	}
 }
