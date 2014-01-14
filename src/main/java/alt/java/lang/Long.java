e81e3aa27f5f41d0d6daae457a3a555f2ebf7269
Ian Hopkins <ian@checkout51.com>
2014-01-14 15:05:09 -0500

    Working on the Long class.

--- src/main/java/alt/java/lang/Long.java
+++ src/main/java/alt/java/lang/Long.java
@@ -1,5 +1,50 @@
 package alt.java.lang;
 
-public class Long {
+public class Long extends Number {
+	
+	private long value;
+	
+	public Long(long value) {
+		this.value = value;
+	}
 
+	public int intValue() {
+		return (int) value;
+	}
+	public long longValue() {
+		return value;
+	}
+	public float floatValue() {
+		return (float) value;
+	}
+	public double doubleValue() {
+		return (double) value;
+	}
+	
+	
+	public String toString() {
+		return Long.toString(value, 10);
+	}
+	
+	public static String toString(long i) {
+		return Long.toString(i, 10);
+	}
+	
+	public static String toString(long i, int radix) {
+		StringBuffer sb = new StringBuffer(65);
+		boolean isNegative = false;
+		if (i == 0) {
+			return "0";
+		} else if (i < 0) {
+			isNegative = true;
+		}
+		while (i > 0) {
+			sb.append(Character.forDigit((int) i % radix, radix));
+			i = i / radix;
+		}
+		if (isNegative) {
+			sb.append('-');
+		}
+		return sb.reverse().toString();
+	}
 }
